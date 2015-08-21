9eaec4f85e8d00e452717f05edf08c94ceff85ab
Matt Allen <cmallen@containerstore.com>
2015-08-21 10:52:29 -0500

    add ch7 directory and files

---
+++ ch7/99_bottles.rb
@@ -0,0 +1,12 @@
+num_bottles = 98
+
+lyrics = <<-song
+			99 bottles of beer on the wall
+			99 bottles of beer
+			take one down, pass it around
+			#{num_bottles} bottles of beer on the wall
+		song
+
+
+
+puts lyrics
