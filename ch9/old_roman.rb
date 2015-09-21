5ce650238256dcadb9e1e987c52f8aa65d477daf
Matt Allen <cmallen@containerstore.com>
2015-09-21 11:08:48 -0500

    finish wip

---
+++ ch9/old_roman.rb
@@ -0,0 +1,21 @@
+def display_roman_numeral numerus, numerals
+	letters = []
+	original = numerus
+	numerals.each do |k, v| 
+		n = numerus.divmod v
+		if n[0] == 0 && n[1] == v
+			next
+		else
+			n[0].times { letters << k }
+			numerus = n[1]
+		end
+	end
+	puts "The number #{original} converted to a Roman numeral is: \n"
+	puts "#{letters.join''}"
+end
+
+
+
+numerals = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 }
+
+display_roman_numeral , numerals
