8faade71b4d9d5be853fda0b4e0c405901c266c8
Matt Allen <cmallen@containerstore.com>
2015-09-28 11:11:18 -0500

    refactor

--- ch9/new_roman.rb
+++ ch9/new_roman.rb
@@ -2,40 +2,36 @@
 def convert_to_roman numerus
 
   letters  = []
-  original = numerus
   numerals = { 
     M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, 
     L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 
   }
 
-  numerals.each do |k, v| 
+  numerals.each do |k, v|
     n = numerus.divmod v
-    if n[0] == 0 && n[1] == v
-      next
-    else
-      n[0].times { letters << k }
-      numerus = n[1]
-    end
+    numerus = n[1]
+    letters << k.to_s * n[0]    
   end
   letters.join
 end
+
 
 
 #Here is another way... monkey patch Fixnum!
 class Fixnum
 
   NUMERALS = {
-    "M" => 1000, "CM" => 900, "D" => 500, "CD" => 400, 
-    "C" => 100, "XC" => 90,  "L" => 50,  "XL" => 40,  
-    "X" => 10, "IX" => 9,   "V" => 5,   "IV" => 4,   
-    "I" => 1
+    :M => 1000, :CM => 900, :D => 500, :CD => 400, 
+    :C => 100, :XC => 90,  :L => 50,  :XL => 40,  
+    :X => 10, :IX => 9,   :V => 5,   :IV => 4,   
+    :I => 1
   }
 
   def romanize
     numerus = self
     NUMERALS.map do |k, v| 
       temp, numerus = numerus.divmod(v)
-      k * temp     
+      k.to_s * temp     
     end.join
   end
 end
