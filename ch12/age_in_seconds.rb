30a1aff45709652992b7638fbe70d806e024b2c8
Matt Allen <cmallen@containerstore.com>
2016-01-07 13:35:12 -0600

    add my birthday

--- ch12/age_in_seconds.rb
+++ ch12/age_in_seconds.rb
@@ -1,6 +1,6 @@
 puts ""
 while true
-	print " kyle's age in seconds is: #{Time.at(Time.now).to_i - Time.utc(2008,"nov",14,13,20,33).to_i} \r"
+	print " Matt's age in seconds is: #{Time.at(Time.now).to_i - Time.utc(1973,"aug",1,13,20,33).to_i} \r"
 	sleep 1
 end
 
